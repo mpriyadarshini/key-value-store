ae1bc05fa2c38cab39624d33213b08beb0f2c810
Megha Priyadarshini <meghapriyadarshini68@gmail.com>
2019-02-28 21:33:11 +0530

    Initial key-value-store implementation

---
+++ test/index.test.js
@@ -0,0 +1,33 @@
+const app = require('../app');
+const { expect } = require('chai');
+const request = require('supertest');
+
+describe('Key Value Store Integration Tests', () => {
+  it('should return "Hello World!"', done => {
+    request(app).get('/')
+      .expect(200)
+      .expect(`"Hello World!"`, done);
+  });
+
+  it('Should save an object by using a key', done => {
+    request(app)
+      .post('/key1')
+      .send('value')
+      .expect(`"OK"`)
+      .expect(200, done);
+  });
+
+  it('Should get an object by its key', done => {
+    request(app)
+      .post('/key1')
+      .send('value')
+      .expect(`"OK"`)
+      .end(() => {
+        request(app)
+          .get('/key1')
+          .expect('value')
+          .expect(200, done);
+      });
+  });
+
+});
